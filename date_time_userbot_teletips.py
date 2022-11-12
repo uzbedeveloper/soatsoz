834f0eb15e924dc59c6d33014df0ba1523f4c914
uzbedeveloper <103754533+uzbedeveloper@users.noreply.github.com>
2022-11-12 19:30:45 +0500

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -16,10 +16,9 @@ import random
 import os
 
 Date_Time_Userbot_teletips=Client(
-    name = "date_time_userbot_teletips",
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
-    session_string = os.environ["SESSION_STRING"]
+    session_name = os.environ["SESSION_NAME"]
 )
 
 Time_Zone = os.environ["TIME_ZONE"]
@@ -31,23 +30,43 @@ async def main_teletips():
                 Quotes_teletips = random.choice(quotes_teletips)
                 Emojis_teletips = random.choice(emojis_teletips)
                 TimeZone_teletips = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
-                Time_teletips = TimeZone_teletips.strftime("%I:%M %p")
-                Date_teletips = TimeZone_teletips.strftime("%b %d") 
-                Image_teletips = Image.open("image.jpg")
+                Time_teletips = TimeZone_teletips.strftime("   %I:%M")
+                Date_teletips = TimeZone_teletips.strftime("%d.%m.%Y") 
+                Image_teletips = Image.open("images2.jpg")
                 Image_font_teletips = ImageFont.truetype("ds-digit.ttf", 360)
+                Image_font_teletips2 = ImageFont.truetype("ds-digit.ttf", 200)
                 Image_text_teletips = f"{Time_teletips}"
+                Image_text_teletips2 = f"{Date_teletips}"
                 Image_edit_teletips = ImageDraw.Draw(Image_teletips)
-                Image_edit_teletips.text((690, 550), Image_text_teletips, (0, 255, 255), font = Image_font_teletips)
+                Image_edit_teletips.text((690, 550), Image_text_teletips, (0,225,225), font = Image_font_teletips)
+                Image_edit_teletips.text((900, 850), Image_text_teletips2, (0,225,225), font = Image_font_teletips2)
                 Image_teletips.save("Image_final_teletips.jpg")
-                await Date_Time_Userbot_teletips.update_profile(bio = f"{Emojis_teletips} {Quotes_teletips}" , last_name = f"| ⏰ {Time_teletips} | 📅 {Date_teletips}")
+                
+                today = datetime.datetime.now()
+                print(today)
+                bday = datetime.datetime(2022,8,29,19,0)
+                print(bday)
+                time_diff = bday - today
+                print(f"Your birthday is in {time_diff}")
+                tdays = time_diff.days
+                print(f"Your birthday is in {tdays} days. abs={abs(int(tdays))}")
+                tsecs = time_diff.total_seconds()
+                print(f"Your birthday is {tsecs} seconds away. and {int(tsecs)} seconds away. abs={abs(int(tsecs))}")
+                tmins = tsecs/60
+                print(f"Your birthday is {tmins} minutes away. and {int(tmins)} minutes away. abs={abs(int(tmins))}")
+                thrs = tsecs/(60*60)
+                print(f"Your birthday is {thrs} hours away. and {int(thrs)} hours away. abs={abs(int(thrs))}")
+             
+                await Date_Time_Userbot_teletips.update_profile(bio = f" " , last_name = f" ")
                 await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
                 me = await Date_Time_Userbot_teletips.get_me()
-                photos = Date_Time_Userbot_teletips.get_chat_photos("me")
+                photos = await Date_Time_Userbot_teletips.get_profile_photos("me")
                 try:
                     await Date_Time_Userbot_teletips.delete_profile_photos(photos[1].file_id)
                 except Exception:
                     pass        
-                print("Profile Updated!")
+                print("Profile Updated! Murodhonov")
+                
             await asyncio.sleep(60)     
     except FloodWait as e:
         await asyncio.sleep(e.x)         
